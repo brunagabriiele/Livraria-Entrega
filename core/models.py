add23502f098baeb24f7f80fe0b457ced194b86c
Bruna Gabriele <rodriguesbruna388@gmail.com>
2022-09-01 21:18:27 -0300

    Livraria - versão de entrega

---
+++ core/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+from django.db import models
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+    def __str__(self):
+        return self.descricao
+
+categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
+
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros"
+    )
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name_plural = "Autores"
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.titulo} ({self.quantidade})'
